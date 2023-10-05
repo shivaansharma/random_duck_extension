4c78927a136b7a9f126da8d205aca8b2ba70c7ca
shivaan sharma <Shivaansharma16@gmail.com>
2023-10-05 20:15:03 +0530

    optamized

--- duck_poop.js
+++ duck_poop.js
@@ -1,22 +1,24 @@
-
 const imageNumber = Math.floor(Math.random() * 200) + 1;
-const gifNumber = Math.floor(Math.random() * 100) + 1;
-console.log(imageNumber);
-
-
+const gifNumber = Math.floor(Math.random() * 50) + 1;
 const gifOrImg = Math.floor(Math.random() * 2) + 1;
-console.log(gifOrImg);
+const div = document.querySelector("div");
 
-let div = document.querySelector("div");
+// Define a common URL prefix
+const urlPrefix = "https://random-d.uk/api/";
 
-if (gifOrImg == 1) {
-    let url = `https://random-d.uk/api/${gifNumber}.gif`;
-    let img = document.createElement("img");
-    img.setAttribute('src', url);
-    div.appendChild(img); 
+// Create an image element
+const img = document.createElement("img");
+img.style.width = '400px'; 
+img.style.height = '400px'; 
+
+// Determine the URL based on gifOrImg
+let url;
+if (gifOrImg === 1) {
+    url = `${urlPrefix}${gifNumber}.gif`;
 } else {
-    let url = `https://random-d.uk/api/${imageNumber}.jpg`;
-    let img = document.createElement("img");
-    img.setAttribute('src', url);
-    div.appendChild(img); 
+    url = `${urlPrefix}${imageNumber}.jpg`;
 }
+
+// Set the src attribute and append the image to the div
+img.setAttribute('src', url);
+div.appendChild(img);
